da000bc2c43d6d9a50147d30ef9ed64a5a20957d
vignesh2201 <147697662+vignesh2201@users.noreply.github.com>
2023-10-12 13:05:59 +0530

    Add files via upload

---
+++ Challenge Unit-2/main.py
@@ -0,0 +1,46 @@
+''''Implment a class called BankAccound that represents a bank Account.  The class should have private
+attributes for account number, account holder name, and account balance. Include methods to 
+deposite money, withdraw money, and display the account balance. Ensure that the account balance 
+cannot be accessed directly from outside the class. Write a program to create an instance of the 
+BankAccount class and test the deposit and withdrawal functionality'''
+
+
+class BankAccount:
+
+  def __init__(self, account_number, account_holder_name, initial_balancee=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balancee
+
+def deposit(self,amount):
+  if amount > 0:
+    self.__account_balance += amount
+  print("Deposited {}. New balance: ${}".format(amount,self.__account_balance))
+
+else:
+print("Invalid deposite amount. Please deposite a positive amount.")
+
+def withdraw(self,amount):
+  if amount > 0 and amount <= self.__account_balance:
+    self.__account_balance -= amount 
+print("Withdraw ${}. New balance: ${}". format(amount,self.__account_balance))
+
+else:
+print("Invalid withdrawal amount or insufficientbalance.")
+
+def display_balance(self):
+  print("Account balance for {} (Account #{}): ${}". format(
+    self.__account_holder_name, self.__account_number,
+    self.__account_balance))
+
+
+# Create an instance of the Banckaccount class
+account = BankAccount(account_number="123456789",
+                      account_holder_name="Hari Prabu",
+                       initial_balancee=5000.0)
+
+# Test deposit and withdrawel functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.display_balance()
